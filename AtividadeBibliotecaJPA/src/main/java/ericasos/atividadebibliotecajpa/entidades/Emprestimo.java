0742e23712b2ea1a49df442de6ba87b206d96a3e
EricaSOS <erica.sos.adv@hotmail.com>
2025-05-13 19:41:35 -0300

    feat: Correções nos imports da Classe Emprestimo

--- AtividadeBibliotecaJPA/src/main/java/ericasos/atividadebibliotecajpa/entidades/Emprestimo.java
+++ AtividadeBibliotecaJPA/src/main/java/ericasos/atividadebibliotecajpa/entidades/Emprestimo.java
@@ -1,47 +1,52 @@
 package ericasos.atividadebibliotecajpa.entidades;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 import java.util.Date;
 
-@Entity 
- 
+@Entity
+@Table(name = "Emprestimo")
 public class Emprestimo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "emprestimo_id")
-    private Long id; 
+    private Long id;
 
     @Column(name = "dataEmprestimo", nullable = false)
-    @Temporal(TemporalType.DATE) 
+    @Temporal(TemporalType.DATE)
     private Date dataEmprestimo;
 
-    @Column(name = "dataDevolucao") 
-    @Temporal(TemporalType.DATE) 
-    private Date dataDevolucao; 
+    @Column(name = "dataDevolucao")
+    @Temporal(TemporalType.DATE)
+    private Date dataDevolucao;
 
-    
-    @ManyToOne 
-    @JoinColumn(name = "aluno_id", nullable = false) 
-    private Aluno aluno; 
+    @ManyToOne
+    @JoinColumn(name = "aluno_id", nullable = false)
+    private Aluno aluno;
 
     @ManyToOne
     @JoinColumn(name = "publicacao_id", nullable = false)
     private Publicacao publicacao;
 
-    
     public Emprestimo() {
-    
     }
 
-    public Emprestimo(Date dataEmprestimo, Date dataDevolucao, Aluno aluno, Publicacao publicacao) {
+    public Emprestimo(Date dataEmprestimo, Aluno aluno, Publicacao publicacao) {
         this.dataEmprestimo = dataEmprestimo;
-        this.dataDevolucao = dataDevolucao;
         this.aluno = aluno;
         this.publicacao = publicacao;
     }
 
-    
     public Long getId() {
         return id;
     }
@@ -88,8 +93,8 @@ public class Emprestimo {
                "id=" + id +
                ", dataEmprestimo=" + dataEmprestimo +
                ", dataDevolucao=" + dataDevolucao +
-               ", aluno=" + (aluno != null ? aluno.getNome() : "null") + 
-               ", publicacao=" + (publicacao != null ? publicacao.getTitulo() : "null") + 
+               ", aluno=" + (aluno != null ? aluno.getNome() : "null") +
+               ", publicacao=" + (publicacao != null ? publicacao.getTitulo() : "null") +
                '}';
     }
 }
