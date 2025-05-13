a2066fd0426faf961e44c8e5881420472a6b2633
EricaSOS <erica.sos.adv@hotmail.com>
2025-05-13 19:23:40 -0300

    feat: correções no Mapeamento das Tabelas e alteração da localização do pacote de testes do Emprestimo DAO

--- AtividadeBibliotecaJPA/Recursos/MapeamentoBibliotecaJPA.sql
+++ AtividadeBibliotecaJPA/Recursos/MapeamentoBibliotecaJPA.sql
@@ -1,12 +1,12 @@
 CREATE TABLE Aluno (
-    aluno_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    matriculaAluno VARCHAR(20) UNIQUE NOT NULL, -- Apesar de aparecer tipo int no diagrama de classes, optei por usar VARCHAR, devido às boas práticas de modelagem
+    aluno_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    matriculaAluno INTEGER UNIQUE NOT NULL,
     nome VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE Publicacao (
-    publicacao_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    codigoPub VARCHAR(20) UNIQUE NOT NULL, -- Apesar de aparecer tipo int no diagrama de classes, optei por usar VARCHAR, devido às boas práticas de modelagem
+    publicacao_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    codigoPub INTEGER UNIQUE NOT NULL,
     titulo VARCHAR(100) NOT NULL,
     ano INT,
     autor VARCHAR(100),
@@ -14,11 +14,11 @@ CREATE TABLE Publicacao (
 );
 
 CREATE TABLE Emprestimo (
-    emprestimo_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    dataEmprestimo DATE NOT NULL,
+    emprestimo_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    dataEmprestimo DATE NOT NULL, 
     dataDevolucao DATE,
-    aluno_id INTEGER NOT NULL, 
-    publicacao_id INTEGER NOT NULL, 
+    aluno_id INTEGER NOT NULL,
+    publicacao_id INTEGER NOT NULL,
     FOREIGN KEY (aluno_id) REFERENCES Aluno(aluno_id),
     FOREIGN KEY (publicacao_id) REFERENCES Publicacao(publicacao_id)
 );
