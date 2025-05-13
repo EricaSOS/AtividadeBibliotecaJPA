fbee77840ba665f04f019d1a58054aa3a9d4d0b7
EricaSOS <erica.sos.adv@hotmail.com>
2025-05-12 22:55:29 -0300

    feat: Inclusão das Classes Empréstimo, Aluno e Publicação no arquivo persistence.xml, bem como exclusão de comentários nos arquivos dessas classes

--- AtividadeBibliotecaJPA/src/main/java/ericasos/atividadebibliotecajpa/entidades/Emprestimo.java
+++ AtividadeBibliotecaJPA/src/main/java/ericasos/atividadebibliotecajpa/entidades/Emprestimo.java
@@ -1,14 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ericasos.atividadebibliotecajpa.entidades;
 
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity 
-@Table(name = "Emprestimo") 
+ 
 public class Emprestimo {
 
     @Id
