1292020fe7ce66d3a6b130beec2d04ab4a2cd659
EricaSOS <erica.sos.adv@hotmail.com>
2025-05-13 21:50:25 -0300

    feat: alteração do arquivo atividadeBibliotecaJPA

--- AtividadeBibliotecaJPA/src/main/java/ericasos/atividadebibliotecajpa/AtividadeBibliotecaJPA.java
+++ AtividadeBibliotecaJPA/src/main/java/ericasos/atividadebibliotecajpa/AtividadeBibliotecaJPA.java
@@ -1,12 +1,34 @@
 package ericasos.atividadebibliotecajpa;
 
-/**
- *
- * @author erica
- */
-public class AtividadeBibliotecaJPA {
+import DAO.EmprestimoDAO;
+import ericasos.atividadebibliotecajpa.entidades.Aluno;
+import ericasos.atividadebibliotecajpa.entidades.Emprestimo;
+import ericasos.atividadebibliotecajpa.entidades.Publicacao;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class AtividadeBibliotecaJPA {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Cria entidades fictícias
+        Aluno aluno = new Aluno();
+        aluno.setNome("Érica Santos");
+        
+        Publicacao publicacao = new Publicacao();
+        publicacao.setTitulo("Inteligência Artificial na Amazônia");
+        
+        Emprestimo emprestimo = new Emprestimo();
+        emprestimo.setAluno(aluno);
+        emprestimo.setPublicacao(publicacao);
+
+        Date dataEmprestimo = Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant());
+        Date dataDevolucao = Date.from(LocalDate.now().plusDays(7).atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        emprestimo.setDataEmprestimo(dataEmprestimo);
+        emprestimo.setDataDevolucao(dataDevolucao);
+        
+        System.out.println("Empréstimo salvo com sucesso!");
     }
 }
+
